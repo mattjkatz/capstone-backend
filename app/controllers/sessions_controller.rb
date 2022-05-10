ab3e4af641023bf7dac449ee960b10a2cbeed0bc
mattjkatz <matthewkatz7@gmail.com>
2022-05-09 22:02:38 -0500

    change the sessions controller to send through more user info

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
         Rails.application.credentials.fetch(:secret_key_base), # the secret key
         "HS256" # the encryption algorithm
       )
-      render json: { jwt: jwt, email: user.email, user_id: user.id }, status: :created
+      render json: { jwt: jwt, email: user.email, user_id: user.id, first_name: user.first_name, last_name: user.last_name, points: user.points }, status: :created
     else
       render json: {}, status: :unauthorized
     end
