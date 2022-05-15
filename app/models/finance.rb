8bd37f4f1230137e922128c2389b25bc65b8f1c4
mattjkatz <matthewkatz7@gmail.com>
2022-05-15 10:32:46 -0500

    remove validation for category in finance create

--- app/models/finance.rb
+++ app/models/finance.rb
@@ -3,7 +3,6 @@ class Finance < ApplicationRecord
   validates :name, presence: true
   validates :name, uniqueness: true
   validates :amount, presence: true
-  validates :category, presence: true
   validates :frequency, presence: true
 
   belongs_to :budget
