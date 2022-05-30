8750238c5e31d1baa36f106f737ebed97199c8ac
mattjkatz <matthewkatz7@gmail.com>
2022-05-30 11:11:01 -0500

    add a second netlify url without main

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:8080", "main--genuine-pavlova-0ae589.netlify.app/"
+    origins "localhost:8080", "genuine-pavlova-0ae589.netlify.app", "main--genuine-pavlova-0ae589.netlify.app"
 
     resource "*",
       headers: :any,
